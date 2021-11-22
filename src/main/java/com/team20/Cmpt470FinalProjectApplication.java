f065c930d38756250ee679980c5ccdd995cf59bb
xubinw <xubinw@sfu.ca>
2021-11-21 18:53:00 -0800

    Empty Files Uploaded

---
+++ src/main/java/com/team20/Cmpt470FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.team20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmpt470FinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cmpt470FinalProjectApplication.class, args);
+    }
+
+}
