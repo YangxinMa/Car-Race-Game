5f2c12d31bdad72daa1bcba2d915817e0c5dd31f
Neil Chen <sjc27@sfu.ca>
2021-11-25 23:19:09 -0800

    Added login page and the dashboard page

---
+++ src/main/java/config/LoginHandlerIntercepter.java
@@ -0,0 +1,26 @@
+package config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginHandlerIntercepter implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        //should have user's session after login successfully
+        Object loginUser =  request.getSession().getAttribute("loginUser");
+        if (loginUser == null){
+            request.setAttribute("msg", "Permission deny, please login first");
+            request.getRequestDispatcher("/index.html").forward(request,response);
+            return false;
+        } else{
+            return true;
+        }
+
+
+    }
+
+
+}
