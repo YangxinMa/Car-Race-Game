b2e1bc020c775c0b6cc109e32d6014ca1c5e6718
Li Liang <lianglil@sfu.ca>
2021-11-26 16:04:57 -0800

    Add sout to check the info

--- src/main/java/com/team20/Controller/MainController.java
+++ src/main/java/com/team20/Controller/MainController.java
@@ -12,6 +12,9 @@ public class MainController {
     @ResponseStatus(HttpStatus.OK)
     public void login(@RequestBody User info) {
         // Check the user in db or not: yes return 200, no return 404
+        // test user info
+        System.out.println(info.username);
+        System.out.println(info.password);
     }
 }
 
